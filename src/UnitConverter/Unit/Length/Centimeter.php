73f9ee4e2a006a2ef9764a7943c437372bd531d7
jordanbrauer <jbrauer.inc@gmail.com>
2017-10-09 00:41:48 -0500

    Feat: add a few default length units. meter is base for all length units

---
+++ src/UnitConverter/Unit/Length/Centimeter.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * This file is part of the jordanbrauer/unit-converter PHP package.
+ *
+ * @copyright 2017 Jordan Brauer <jbrauer.inc@gmail.com>
+ * @license MIT
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
+namespace UnitConverter\Unit\Length;
+
+use UnitConverter\Measure;
+use UnitConverter\Unit\{ AbstractUnit, UnitInterface };
+
+/**
+ * Centimeter data class.
+ *
+ * @version 1.0.0
+ * @since 1.0.0
+ * @author Jordan Brauer <jbrauer.inc@gmail.com>
+ */
+class Centimeter extends LengthUnit
+{
+  protected $name = "centimeter";
+
+  protected $symbol = "cm";
+
+  protected $units = 0.01;
+}
