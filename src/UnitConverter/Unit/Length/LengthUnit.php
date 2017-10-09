75c53423f6a7d040653f9107a07519ebb04f9b5c
jordanbrauer <jbrauer.inc@gmail.com>
2017-10-09 01:01:08 -0500

    Fix: included missing base LengthUnit file

---
+++ src/UnitConverter/Unit/Length/LengthUnit.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * This file is part of the jordanbrauer/unit-converter PHP package.
+ *
+ * @copyright 2017 Jordan Brauer <jbrauer.inc@gmail.com>
+ * @license MIT
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
+namespace UnitConverter\Unit\Length;
+
+use UnitConverter\Measure;
+use UnitConverter\Unit\{ AbstractUnit, UnitInterface };
+
+/**
+ * Length unit base data class. Any new length units should
+ * extend this class and implement their name, symbol and units;
+ * overriding the $unitOf and $base properties only if necessary.
+ *
+ * @version 1.0.0
+ * @since 1.0.0
+ * @author Jordan Brauer <jbrauer.inc@gmail.com>
+ */
+class LengthUnit extends AbstractUnit
+{
+  protected $unitOf = Measure::LENGTH;
+
+  protected $base = Meter::class;
+}
