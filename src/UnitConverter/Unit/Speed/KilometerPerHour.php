264b03c2118b0e7bc7e167616f8b25b8452c653d
jordanbrauer <jbrauer.inc@gmail.com>
2017-10-10 09:47:41 -0500

    Feat: add support for kilometer per hour units

---
+++ src/UnitConverter/Unit/Speed/KilometerPerHour.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * This file is part of the jordanbrauer/unit-converter PHP package.
+ *
+ * @copyright 2017 Jordan Brauer <jbrauer.inc@gmail.com>
+ * @license MIT
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
+namespace UnitConverter\Unit\Speed;
+
+use UnitConverter\Measure;
+use UnitConverter\Unit\{ AbstractUnit, UnitInterface };
+
+/**
+ * Kilometer per hour unit data class.
+ *
+ * @version 1.0.0
+ * @since 1.0.0
+ * @author Jordan Brauer <jbrauer.inc@gmail.com>
+ */
+class KilometerPerHour extends SpeedUnit
+{
+  protected $name = "kilometer per hour";
+
+  protected $symbol = "kph";
+
+  protected $units = 0.44704;
+}
