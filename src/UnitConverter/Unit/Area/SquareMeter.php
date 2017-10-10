0203302b1365017d65202d389feec80dace39385
jordanbrauer <jbrauer.inc@gmail.com>
2017-10-09 19:28:14 -0500

    Feat: square meter units support.

---
+++ src/UnitConverter/Unit/Area/SquareMeter.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * This file is part of the jordanbrauer/unit-converter PHP package.
+ *
+ * @copyright 2017 Jordan Brauer <jbrauer.inc@gmail.com>
+ * @license MIT
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
+namespace UnitConverter\Unit\Area;
+
+use UnitConverter\Measure;
+use UnitConverter\Unit\{ AbstractUnit, UnitInterface };
+
+/**
+ * Area unit base data class. Any new area units should
+ * extend this class and implement their name, symbol and units;
+ * overriding the $unitOf and $base properties only if necessary.
+ *
+ * @version 1.0.0
+ * @since 1.0.0
+ * @author Jordan Brauer <jbrauer.inc@gmail.com>
+ */
+abstract class SquareMeter extends AreaUnit
+{
+  protected $name = "square meter";
+
+  protected $symbol = "m2";
+
+  protected $units = 1;
+}
