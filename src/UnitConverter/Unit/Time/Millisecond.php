d07a0451484264b1c86375186df33716bc61d387
DESKTOP-8RHQV24\Teun <teun.willems@live.nl>
2017-10-11 22:49:01 +0200

    Added time measurements

---
+++ src/UnitConverter/Unit/Time/Millisecond.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * This file is part of the jordanbrauer/unit-converter PHP package.
+ *
+ * @copyright 2017 Jordan Brauer <jbrauer.inc@gmail.com>
+ * @license MIT
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
+namespace UnitConverter\Unit\Volume;
+
+use UnitConverter\Unit\{
+    Time\TimeUnit
+};
+
+/**
+ * Millisecond unit data class.
+ *
+ * @version 1.0.0
+ * @since 1.0.0
+ * @author Teun Willems
+ */
+class Millisecond extends TimeUnit
+{
+    protected function configure () : void
+    {
+        $this
+            ->setName("millisecond")
+
+            ->setSymbol("ms")
+
+            ->setUnits(.001)
+        ;
+    }
+}
