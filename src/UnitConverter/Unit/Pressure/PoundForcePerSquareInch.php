ae912084f44678143258a0667733c3e4f9970308
jordanbrauer <jbrauer.inc@gmail.com>
2017-10-10 21:28:49 -0500

    Feat: add support for basic pressure units w/ single integration test

---
+++ src/UnitConverter/Unit/Pressure/PoundForcePerSquareInch.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * This file is part of the jordanbrauer/unit-converter PHP package.
+ *
+ * @copyright 2017 Jordan Brauer <jbrauer.inc@gmail.com>
+ * @license MIT
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
+namespace UnitConverter\Unit\Pressure;
+
+use UnitConverter\Measure;
+use UnitConverter\Unit\{ AbstractUnit, UnitInterface };
+
+/**
+ * PoundForcePerSquareInch unit data class.
+ *
+ * @version 1.0.0
+ * @since 1.0.0
+ * @author Jordan Brauer <jbrauer.inc@gmail.com>
+ */
+class PoundForcePerSquareInch extends PressureUnit
+{
+  protected function configure () : void
+  {
+    $this
+      ->setName("pound-force per square inch")
+
+      ->setSymbol("psi")
+
+      ->setUnits(6894.76)
+      ;
+  }
+}
