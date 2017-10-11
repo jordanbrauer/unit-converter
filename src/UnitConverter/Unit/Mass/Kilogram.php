d883ec615f3c286a9a34f63484f9aab321362297
jordanbrauer <jbrauer.inc@gmail.com>
2017-10-10 22:06:48 -0500

    Feat: add kilogram unit support

---
+++ src/UnitConverter/Unit/Mass/Kilogram.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * This file is part of the jordanbrauer/unit-converter PHP package.
+ *
+ * @copyright 2017 Jordan Brauer <jbrauer.inc@gmail.com>
+ * @license MIT
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
+namespace UnitConverter\Unit\Mass;
+
+use UnitConverter\Measure;
+use UnitConverter\Unit\{ AbstractUnit, UnitInterface };
+
+/**
+ * Kilogram data class.
+ *
+ * @version 1.0.0
+ * @since 1.0.0
+ * @author Jordan Brauer <jbrauer.inc@gmail.com>
+ */
+class Kilogram extends MassUnit
+{
+  protected function configure () : void
+  {
+    $this
+      ->setName("kilogram")
+
+      ->setSymbol("kg")
+
+      ->setUnits(1)
+      ;
+  }
+}
