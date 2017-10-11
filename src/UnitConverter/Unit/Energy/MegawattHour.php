6b9c0a0ccb6d82ffd42627410257618386c0ab24
andreboerema <1569403+andrewboerema@users.noreply.github.com>
2017-10-11 12:31:49 -0500

    Adding requested energy units

---
+++ src/UnitConverter/Unit/Energy/MegawattHour.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * This file is part of the jordanbrauer/unit-converter PHP package.
+ *
+ * @copyright 2017 Jordan Brauer <jbrauer.inc@gmail.com>
+ * @license MIT
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
+namespace UnitConverter\Unit\Energy;
+
+use UnitConverter\Measure;
+use UnitConverter\Unit\{ AbstractUnit, UnitInterface };
+
+/**
+ * MegawattHour unit data class.
+ *
+ * @version 1.0.0
+ * @since 1.0.0
+ * @author Andrew Boerema <1569403+andrewboerema@users.noreply.github.com>
+ */
+class MegawattHour extends EnergyUnit
+{
+  protected function configure () : void
+  {
+    $this
+      ->setName("megawatt hour")
+
+      ->setSymbol("MWh")
+
+      ->setUnits(3600005446.8)
+      ;
+  }
+}
---
+++ src/UnitConverter/Unit/Energy/NewtonMeter.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * This file is part of the jordanbrauer/unit-converter PHP package.
+ *
+ * @copyright 2017 Jordan Brauer <jbrauer.inc@gmail.com>
+ * @license MIT
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
+namespace UnitConverter\Unit\Energy;
+
+use UnitConverter\Measure;
+use UnitConverter\Unit\{ AbstractUnit, UnitInterface };
+
+/**
+ * NewtonMeter unit data class.
+ *
+ * @version 1.0.0
+ * @since 1.0.0
+ * @author Andrew Boerema <1569403+andrewboerema@users.noreply.github.com>
+ */
+class NewtonMeter extends EnergyUnit
+{
+  protected function configure () : void
+  {
+    $this
+      ->setName("newton meter")
+
+      ->setSymbol("Nm")
+
+      ->setUnits(1)
+      ;
+  }
+}
