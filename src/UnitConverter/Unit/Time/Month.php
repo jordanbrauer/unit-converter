611293e92b13b659ac26c8bde6c8559ffc03a742
Teun Willems <teun.willems+github@live.nl>
2017-10-12 20:22:31 +0200

    Added time measurements (#35)
    Closes #10 
    
    * Added time measurements
    
    * Added requested changes
    
    * Added new time units

---
+++ src/UnitConverter/Unit/Time/Month.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * This file is part of the jordanbrauer/unit-converter PHP package.
+ *
+ * @copyright 2017 Jordan Brauer <jbrauer.inc@gmail.com>
+ * @license MIT
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
+namespace UnitConverter\Unit\Time;
+
+/**
+ * Month unit data class.
+ *
+ * @version 1.0.0
+ * @since 1.0.0
+ * @author Teun Willems
+ */
+class Month extends TimeUnit
+{
+    protected function configure () : void
+    {
+        $this
+            ->setName("month")
+
+            ->setSymbol("month")
+
+            ->setUnits(2678400)
+        ;
+    }
+}
