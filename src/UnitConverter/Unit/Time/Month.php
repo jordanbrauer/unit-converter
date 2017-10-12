9c8e0f2ee90c78dd7fe57f62f1893b28103f3082
DESKTOP-8RHQV24\Teun <teun.willems@live.nl>
2017-10-12 20:08:30 +0200

    Added concept for calculatable months

--- src/UnitConverter/Unit/Time/Month.php
+++ src/UnitConverter/Unit/Time/Month.php
@@ -13,6 +13,7 @@
 declare(strict_types = 1);
 
 namespace UnitConverter\Unit\Time;
+use UnitConverter\Unit\UnitInterface;
 
 /**
  * Month unit data class.
@@ -32,5 +33,22 @@ class Month extends TimeUnit
 
             ->setUnits(2678400)
         ;
+    }
+
+    private function has31Days($month) {
+        return is_numeric(array_search($month, [1, 3, 5, 7, 8, 10, 12]));
+    }
+
+    public function calculate(float $value, UnitInterface $to, $year = null): ?float
+    {
+        if ($year == null) $year = date('Y');
+
+        if ($to->getSymbol() == 'day') {
+            if ($value == 2) {
+                return Year::isLeapYear($year) ? 29.0 : 28.0;
+            }
+            return $this->has31Days($value) ? 31.0 : 30.0;
+        }
+        throw new \Exception("Could not find unit to convert to");
     }
 }
--- src/UnitConverter/Unit/Time/Year.php
+++ src/UnitConverter/Unit/Time/Year.php
@@ -10,7 +10,7 @@
  * file that was distributed with this source code.
  */
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace UnitConverter\Unit\Time;
 
@@ -23,14 +23,29 @@ namespace UnitConverter\Unit\Time;
  */
 class Year extends TimeUnit
 {
-    protected function configure () : void
+    protected function configure(): void
     {
         $this
             ->setName("year")
-
             ->setSymbol("year")
+            ->setUnits(31536000);
+    }
 
-            ->setUnits(31536000)
-        ;
+    public static function isLeapYear($year = null)
+    {
+        if ($year == null) $year = date('Y');
+
+        // Every 4 years is a leap year
+        if ($year % 4 == 0) {
+            // Exception when divisible by 100 but not 400
+            if ($year % 100 == 0) {
+                if ($year % 400 == 0) {
+                    return true;
+                }
+                return false;
+            }
+            return true;
+        }
+        return false;
     }
 }
