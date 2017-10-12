9b7b10b998a95bfe5e8a5229f2080cfe4522e68e
DESKTOP-8RHQV24\Teun <teun.willems@live.nl>
2017-10-12 19:35:52 +0200

    Added new time units

---
+++ src/UnitConverter/Unit/Time/Nanosecond.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * This file is part of the jordanbrauer/unit-converter PHP package.
+ *
+ * @copyright 2017 Jordan Brauer <jbrauer.inc@gmail.com>
+ * @license MIT
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
+namespace UnitConverter\Unit\Time;
+
+/**
+ * Nanosecond unit data class.
+ *
+ * @version 1.0.0
+ * @since 1.0.0
+ * @author Teun Willems
+ */
+class Nanosecond extends TimeUnit
+{
+    protected function configure () : void
+    {
+        $this
+            ->setName("nanosecond")
+
+            ->setSymbol("ns")
+
+            ->setUnits(0.000000001)
+        ;
+    }
+}
