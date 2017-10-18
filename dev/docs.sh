ca7469723caf27a1c9a6791977907b7333a2d7fd
Jordan Brauer <jbrauer.inc@gmail.com>
2017-10-18 08:44:19 -0500

    Add first set of dev docs (#44)
    * Update: composer docs cmd now uses bash script to check for stale docs
    
    * Update: phpDocumentor XML config file
    
    * Feat: docs for v0.3.9-alpha (#43)
    
    * Update: move docs directory to project root for github pages support

---
+++ dev/docs.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# This script simply checks for and deletes stale docs
+# before generating a new set.
+
+# NOTE: You may need to set executable permissions before
+# using this file or the `$ composer docs` script.
+#
+# $ chmod u+x ./dev/docs.sh
+
+DIRECTORY=./docs
+
+if [ -d $DIRECTORY ];
+then
+  printf "Deleting old generated documentation .. "
+
+  rm -r $DIRECTORY/*
+
+  echo "OK"
+
+  exit
+fi
+
+echo "No stale documentation to remove"
+
+exit
