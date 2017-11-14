c4b889eb8ffb4dfd858f6e9b24d226c8edee8b3e
jordanbrauer <jbrauer.inc@gmail.com>
2017-11-14 10:46:30 -0600

    Update: add missing length unit integreation tests

---
+++ tests/integration/UnitConverter/Unit/Length/AstronomicalUnit.spec.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * This file is part of the jordanbrauer/unit-converter PHP package.
+ *
+ * @copyright 2017 Jordan Brauer <jbrauer.inc@gmail.com>
+ * @license MIT
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare (strict_types = 1);
+
+namespace UnitConverter\Tests\Integration\Unit\Length;
+
+use PHPUnit\Framework\TestCase;
+use UnitConverter\UnitConverter;
+use UnitConverter\Calculator\SimpleCalculator;
+use UnitConverter\Registry\UnitRegistry;
+use UnitConverter\Unit\Length\Metre;
+use UnitConverter\Unit\Length\AstronomicalUnit;
+
+/**
+ * Ensure that an astronomical unit is an astronomical unit.
+ *
+ * @covers UnitConverter\Unit\Length\AstronomicalUnit
+ * @uses UnitConverter\Unit\Length\Metre
+ * @uses UnitConverter\Unit\AbstractUnit
+ * @uses UnitConverter\UnitConverter
+ * @uses UnitConverter\Calculator\SimpleCalculator
+ * @uses UnitConverter\Calculator\AbstractCalculator
+ * @uses UnitConverter\Registry\UnitRegistry
+ */
+class AstronomicalUnitSpec extends TestCase
+{
+    protected function setUp ()
+    {
+        $this->converter = new UnitConverter(
+            new UnitRegistry(array(
+                new Metre,
+                new AstronomicalUnit,
+            )),
+            new SimpleCalculator
+        );
+    }
+
+    protected function tearDown ()
+    {
+        unset($this->converter);
+    }
+
+    /**
+     * @test
+     */
+    public function assert1AstronomicalUnitIs149597870700Metres ()
+    {
+        $expected = 149597870700;
+        $actual = $this->converter
+            ->convert(1)
+            ->from("au")
+            ->to("m")
+            ;
+
+        $this->assertEquals($expected, $actual);
+    }
+}
---
+++ tests/integration/UnitConverter/Unit/Length/Kilometre.spec.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * This file is part of the jordanbrauer/unit-converter PHP package.
+ *
+ * @copyright 2017 Jordan Brauer <jbrauer.inc@gmail.com>
+ * @license MIT
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare (strict_types = 1);
+
+namespace UnitConverter\Tests\Integration\Unit\Length;
+
+use PHPUnit\Framework\TestCase;
+use UnitConverter\UnitConverter;
+use UnitConverter\Calculator\SimpleCalculator;
+use UnitConverter\Registry\UnitRegistry;
+use UnitConverter\Unit\Length\Metre;
+use UnitConverter\Unit\Length\Kilometre;
+
+/**
+ * Ensure that a kilometre is infact, a kilometre.
+ *
+ * @covers UnitConverter\Unit\Length\Kilometre
+ * @uses UnitConverter\Unit\Length\Metre
+ * @uses UnitConverter\Unit\AbstractUnit
+ * @uses UnitConverter\UnitConverter
+ * @uses UnitConverter\Calculator\SimpleCalculator
+ * @uses UnitConverter\Calculator\AbstractCalculator
+ * @uses UnitConverter\Registry\UnitRegistry
+ */
+class KilometreSpec extends TestCase
+{
+    protected function setUp ()
+    {
+        $this->converter = new UnitConverter(
+            new UnitRegistry(array(
+                new Metre,
+                new Kilometre,
+            )),
+            new SimpleCalculator
+        );
+    }
+
+    protected function tearDown ()
+    {
+        unset($this->converter);
+    }
+
+    /**
+     * @test
+     */
+    public function assert1KilometreIs1000Metres ()
+    {
+        $expected = 1000;
+        $actual = $this->converter
+            ->convert(1)
+            ->from("km")
+            ->to("m")
+            ;
+
+        $this->assertEquals($expected, $actual);
+    }
+}
