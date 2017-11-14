e7145c9d319c3090a8c0b97f1e017e48361d5a08
jordanbrauer <jbrauer.inc@gmail.com>
2017-11-14 11:02:42 -0600

    Update: add missing tests for plane angle units of measure

---
+++ tests/integration/UnitConverter/Unit/PlaneAngle/Radian.spec.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * This file is part of the jordanbrauer/unit-converter PHP package.
+ *
+ * @copyright 2017 Jordan Brauer <jbrauer.inc@gmail.com>
+ * @license MIT
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare (strict_types = 1);
+
+namespace UnitConverter\Tests\Integration\Unit\PlaneAngle;
+
+use PHPUnit\Framework\TestCase;
+use UnitConverter\UnitConverter;
+use UnitConverter\Calculator\SimpleCalculator;
+use UnitConverter\Registry\UnitRegistry;
+use UnitConverter\Unit\PlaneAngle\Degree;
+use UnitConverter\Unit\PlaneAngle\Radian;
+
+/**
+ * Ensure that a radian is infact, a radian.
+ *
+ * @covers UnitConverter\Unit\PlaneAngle\Radian
+ * @uses UnitConverter\Unit\PlaneAngle\Degree
+ * @uses UnitConverter\Unit\AbstractUnit
+ * @uses UnitConverter\UnitConverter
+ * @uses UnitConverter\Calculator\SimpleCalculator
+ * @uses UnitConverter\Calculator\AbstractCalculator
+ * @uses UnitConverter\Registry\UnitRegistry
+ */
+class RadianSpec extends TestCase
+{
+    protected function setUp ()
+    {
+        $this->converter = new UnitConverter(
+            new UnitRegistry(array(
+                new Degree,
+                new Radian,
+            )),
+            new SimpleCalculator
+        );
+    }
+
+    protected function tearDown ()
+    {
+        unset($this->converter);
+    }
+
+    /**
+     * @test
+     */
+    public function assert1DegreeIs57decimal2958Degrees ()
+    {
+        $expected = 57.2958;
+        $actual = $this->converter
+            ->convert(1, 4)
+            ->from("rad")
+            ->to("deg")
+            ;
+
+        $this->assertEquals($expected, $actual);
+    }
+}
