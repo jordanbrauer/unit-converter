142026a3de9a7f611a71e9d3765406b88d10b574
Jordan Brauer <jorbgit@gmail.com>
2018-02-04 17:45:55 -0600

    Fix code styles (#71), closes #67
    * Update: codestyle for strict type declarations
    
    * Update: copyright year from 2017 to 2018
    
    * Update: add nullable return types to appropriate methods

--- src/UnitConverter/Unit/UnitInterface.php
+++ src/UnitConverter/Unit/UnitInterface.php
@@ -1,16 +1,14 @@
-<?php
+<?php declare(strict_types = 1);
 
 /**
  * This file is part of the jordanbrauer/unit-converter PHP package.
  *
- * @copyright 2017 Jordan Brauer <jbrauer.inc@gmail.com>
+ * @copyright 2018 Jordan Brauer <jbrauer.inc@gmail.com>
  * @license MIT
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
-declare (strict_types = 1);
 
 namespace UnitConverter\Unit;
 
@@ -36,7 +34,7 @@ interface UnitInterface
      *
      * @return string
      */
-    public function getName (): string;
+    public function getName (): ?string;
 
     /**
      * Sets the symbol notation used for the unit.
@@ -51,7 +49,7 @@ interface UnitInterface
      *
      * @return string
      */
-    public function getSymbol (): string;
+    public function getSymbol (): ?string;
 
     /**
      * Sets the unicode scientific symbol notation used for the unit.
@@ -66,7 +64,7 @@ interface UnitInterface
      *
      * @return string
      */
-    public function getScientificSymbol (): string;
+    public function getScientificSymbol (): ?string;
 
     /**
      * Sets the type of measurement that this unit is measuring.
@@ -81,7 +79,7 @@ interface UnitInterface
      *
      * @return string
      */
-    public function getUnitOf (): string;
+    public function getUnitOf (): ?string;
 
     /**
      * Sets the unit class that this unit is based off of.
@@ -98,7 +96,7 @@ interface UnitInterface
      *
      * @return UnitInterface
      */
-    public function getBase (): UnitInterface;
+    public function getBase (): ?UnitInterface;
 
     /**
      * Sets the amount of base units required to make up 1 of the unit.
@@ -113,12 +111,12 @@ interface UnitInterface
      *
      * @return float
      */
-    public function getUnits (): float;
+    public function getUnits (): ?float;
 
     /**
      * Returns the units base unit units'.
      *
      * @return float
      */
-    public function getBaseUnits (): float;
+    public function getBaseUnits (): ?float;
 }
--- tests/unit/UnitConverter/Calculator/BinaryCalculator.spec.php
+++ tests/unit/UnitConverter/Calculator/BinaryCalculator.spec.php
@@ -1,22 +1,19 @@
-<?php
+<?php declare(strict_types = 1);
 
 /**
  * This file is part of the jordanbrauer/unit-converter PHP package.
  *
- * @copyright 2017 Jordan Brauer <jbrauer.inc@gmail.com>
+ * @copyright 2018 Jordan Brauer <jbrauer.inc@gmail.com>
  * @license MIT
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
 
-declare (strict_types = 1);
-
 namespace UnitConverter\Tests\Unit\Calculator;
 
 use PHPUnit\Framework\TestCase;
 use UnitConverter\Calculator\BinaryCalculator;
-
 
 /**
  * @coversDefaultClass UnitConverter\Calculator\BinaryCalculator
