3af75fabc0e4e9718b459191d29cac7cd48c9474
Jordan Brauer <18744334+jordanbrauer@users.noreply.github.com>
2018-05-05 18:46:28 -0500

    Update release command to use helper methods

--- RoboFile.php
+++ RoboFile.php
@@ -31,11 +31,10 @@ class RoboFile extends Tasks
      */
     public function releaseStable (string $version, bool $commit = true): void
     {
-        $commit = false;
-        // TODO: bump version(s) in files
-
+        $this->checkoutMaster($commit);
         $this->upgradeDocumentation($version, $commit);
         $this->upgradeChangelog($version, $commit);
+        $this->tagRelease($version, $commit);
         $this->say("Successfully bumped version to <fg=blue>v</><fg=cyan>{$version}</>");
     }
 
