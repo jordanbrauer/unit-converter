ad9fd7f19248b6e03d79dcb5b98574ac124bdfe6
Jordan Brauer <jorbgit@gmail.com>
2018-05-05 19:23:36 -0500

    Revert "Formatting fixes"
    This reverts commit ec44d551753aa8897990c98fb934027cb3aa574b.

--- RoboFile.php
+++ RoboFile.php
@@ -35,8 +35,6 @@ class RoboFile extends Tasks
         $this->upgradeDocumentation($version, $commit);
         $this->upgradeChangelog($version, $commit);
         $this->tagRelease($version, $commit);
-
-        echo PHP_EOL;
         $this->say("Successfully bumped version to <fg=blue>v</><fg=cyan>{$version}</>");
     }
 
@@ -47,7 +45,7 @@ class RoboFile extends Tasks
      * @param boolean $commitTag (optional) Commit the tag or not.
      * @return void
      */
-    public function tagRelease (string $version, bool $commitTag = true)
+    public function tagRelease(string $version, bool $commitTag = true)
     {
         if ($commitTag) {
             $this->taskGitStack()
