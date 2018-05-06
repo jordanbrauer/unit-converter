798107c5bfefaea01adc54526067cafb1b2781fb
Jordan Brauer <jorbgit@gmail.com>
2018-05-05 19:23:36 -0500

    Revert "Codestyle fix for robo task file"
    This reverts commit b2ba24e39ae8fdbd2476a71338bbcf0aa23c5953.

--- RoboFile.php
+++ RoboFile.php
@@ -1,15 +1,10 @@
 <?php
-
 /**
  * This is project's console commands configuration for Robo task runner.
  *
  * @see http://robo.li/
  */
-
-use Robo\Tasks;
-
-class RoboFile extends Tasks
-{
+class RoboFile extends \Robo\Tasks
 {
     // define public methods as commands
 }
