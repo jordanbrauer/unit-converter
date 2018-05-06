a953f8e1f8b0a15ee4fd0990bae99909fd57d417
Jordan Brauer <jorbgit@gmail.com>
2018-05-05 19:23:36 -0500

    Revert "Add changelog generator method & documentation helper methods"
    This reverts commit 94c037ad6c2a8a39a1b02f12cde564b17b9e7067.

--- RoboFile.php
+++ RoboFile.php
@@ -10,43 +10,6 @@ use Robo\Tasks;
 
 class RoboFile extends Tasks
 {
-    const DOCUMENTATION_BIN = 'bin/phpdoc.phar';
-
-    const DOCUMENTATION_ROOT = 'docs';
-
-    /**
-     * Generate a new copy of the changelog.
-     *
-     * @link https://github.com/skywinder/github-changelog-generator
-     *
-     * @return void
-     */
-    public function generateChangelog ()
-    {
-        $this->_exec('github_changelog_generator');
-    }
-    /**
-     * Returns the root path for the documentation to be generated in.
-     *
-     * @param null|string (optional) A custom path to generate the docs to.
-     * @return string
-     */
-    private function documentationRoot (string $destination = null): string
-    {
-        $root = self::DOCUMENTATION_ROOT;
-
-        return ($destination)
-            ? rtrim($destination, '/').'/'.$root
-            : "{$this->rootPath()}/{$root}";
-    }
-
-    /**
-     * Returns the current root path of the repository.
-     *
-     * @return string
-     */
-    private function rootPath (): string
 {
-        return rtrim(realpath(__DIR__), '/');
-    }
+    // define public methods as commands
 }
