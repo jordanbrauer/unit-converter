d9608de88498d76aebf041d0964d077fc36968c8
Jordan Brauer <18744334+jordanbrauer@users.noreply.github.com>
2018-08-05 10:56:36 -0500

    Update use annotations for covergae

--- tests/integration/UnitConverter/Unit/Area/Hectare.spec.php
+++ tests/integration/UnitConverter/Unit/Area/Hectare.spec.php
@@ -29,6 +29,8 @@ use UnitConverter\Unit\Area\Hectare;
  * @uses UnitConverter\Calculator\SimpleCalculator
  * @uses UnitConverter\Calculator\AbstractCalculator
  * @uses UnitConverter\Registry\UnitRegistry
+ * @uses UnitConverter\Support\ArrayDotNotation
+ * @uses UnitConverter\Support\Collection
  */
 class HectareSpec extends TestCase
 {
