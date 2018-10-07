b1e1eb94977d20c1ed4665908621e26556c93611
Jessica Mauerhan <jessicamauerhan@gmail.com>
2018-10-07 14:29:26 -0500

    added millihertz

---
+++ tests/integration/UnitConverter/Unit/Frequency/Millihertz.spec.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types = 1);
+
+/**
+ * This file is part of the jordanbrauer/unit-converter PHP package.
+ *
+ * @copyright 2018 Jordan Brauer <jbrauer.inc@gmail.com>
+ * @license MIT
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace UnitConverter\Tests\Integration\Unit\Frequency;
+
+use PHPUnit\Framework\TestCase;
+use UnitConverter\Calculator\SimpleCalculator;
+use UnitConverter\Registry\UnitRegistry;
+use UnitConverter\Unit\Frequency\Millihertz;
+use UnitConverter\Unit\Frequency\Hertz;
+use UnitConverter\UnitConverter;
+
+/**
+ * Ensure that a millihertz is infact, a millihertz.
+ *
+ * @covers UnitConverter\Unit\Frequency\Millihertz
+ * @uses UnitConverter\Unit\Frequency\Hertz
+ * @uses UnitConverter\Unit\AbstractUnit
+ * @uses UnitConverter\UnitConverter
+ * @uses UnitConverter\Calculator\SimpleCalculator
+ * @uses UnitConverter\Calculator\AbstractCalculator
+ * @uses UnitConverter\Registry\UnitRegistry
+ * @uses UnitConverter\Support\ArrayDotNotation
+ * @uses UnitConverter\Support\Collection
+ */
+class MillihertzSpec extends TestCase
+{
+    protected function setUp()
+    {
+        $this->converter = new UnitConverter(
+            new UnitRegistry([
+                new Hertz(),
+                new Millihertz(),
+            ]),
+            new SimpleCalculator()
+        );
+    }
+
+    protected function tearDown()
+    {
+        unset($this->converter);
+    }
+
+    /**
+     * @test
+     */
+    public function assert1MillihertzIs0decimal001Hertzs()
+    {
+		$expected = 0.001;
+        $actual = $this->converter
+            ->convert(1, 3)
+            ->from("mHz")
+            ->to("Hz");
+
+        $this->assertEquals($expected, $actual);
+    }
+}
