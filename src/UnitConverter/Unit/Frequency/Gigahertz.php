35f30092882370e6769993a387602427088de440
Jessica Mauerhan <jessicamauerhan@gmail.com>
2018-10-07 14:25:18 -0500

    Added Gigahertz

---
+++ src/UnitConverter/Unit/Frequency/Gigahertz.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types = 1);
+
+/**
+ * This file is part of the jordanbrauer/unit-converter PHP package.
+ *
+ * @copyright 2018 Jordan Brauer <jbrauer.inc@gmail.com>
+ * @license MIT
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace UnitConverter\Unit\Frequency;
+
+use UnitConverter\Unit\SiMultipleUnitInterface;
+
+/**
+ * Gigahertz data class.
+ *
+ * @version 2.0.0
+ * @since 0.0.1
+ * @author Jordan Brauer <jbrauer.inc@gmail.com>
+ */
+class Gigahertz extends FrequencyUnit
+{
+    protected function configure(): void
+    {
+        $this
+            ->setName("gigahertz")
+
+            ->setSymbol("GHz")
+
+            ->setUnits(1000000000);
+    }
+}
