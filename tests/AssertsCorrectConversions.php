b9eebcfdc1dcf02d1c6cfe7c1aae23c3bdeeb32e
Jordan Brauer <18744334+jordanbrauer@users.noreply.github.com>
2019-07-25 21:06:26 -0500

    dynamic converter for incoming from unit

--- tests/AssertsCorrectConversions.php
+++ tests/AssertsCorrectConversions.php
@@ -4,14 +4,15 @@ declare(strict_types = 1);
 
 namespace UnitConverter\Tests;
 
+use UnitConverter\Measure;
 use UnitConverter\Unit\UnitInterface;
+use UnitConverter\UnitConverter;
 
 trait AssertsCorrectConversions
 {
     /**
      * @test
      * @dataProvider correctConversions
-     *
      * @param int|float|string $amount
      * @param UnitInterface $from
      * @param int|float|string $expected
@@ -20,10 +21,26 @@ trait AssertsCorrectConversions
      */
     public function assertCorrectConversions($amount, UnitInterface $from, $expected, UnitInterface $to, int $precision = null): void
     {
-        $converter = $this->simpleVolumeConverter();
+        $this->assertSame($from->getUnitOf(), $to->getUnitOf(), 'Cannot convert units that do not share a measurement');
+
+        $converter = $this->determineConverter($from);
         $actual = $converter->convert($amount, $precision)->from($from->getSymbol())->to($to->getSymbol());
 
         $this->assertEquals($expected, $actual);
         $this->assertSame($expected, $actual);
+    }
+
+    /**
+     * Return the converter for a given unit.
+     *
+     * @param UnitInterface $from
+     * @return UnitConverter
+     */
+    private function determineConverter(UnitInterface $from): UnitConverter
+    {
+        switch ($from->getUnitOf()) {
+            case Measure::VOLUME:
+                return $this->simpleVolumeConverter();
+        }
     }
 }
