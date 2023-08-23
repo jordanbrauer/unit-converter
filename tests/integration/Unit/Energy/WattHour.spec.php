a5781621e0cd445ce99f5223c93a3cc5c05acfb0
jordanbrauer <18744334+jordanbrauer@users.noreply.github.com>
2023-08-23 19:29:15 -0400

    fix incorrect assertions and skip yd -> au test

--- tests/integration/Unit/Energy/WattHour.spec.php
+++ tests/integration/Unit/Energy/WattHour.spec.php
@@ -59,7 +59,7 @@ final class WattHourSpec extends TestCase
             '1 watt hour is equal to 0.000001 megawatt hours'     => [$wh, new MegawattHour(0.000001), 6],
             '1 watt hour is equal to 0.001 kilowatt hours'        => [$wh, new KilowattHour(0.001), 3],
             '1 watt hour is equal to 0.85984522785899 calories'   => [$wh, new Calorie(0.860421), 6],
-            '1 watt hour is equal to 2655.2237373982 foot pounds' => [$wh, new FootPound(2655.2237373982), 20],
+            '1 watt hour is equal to 2655.2237373982 foot pounds' => [$wh, new FootPound(2655.2237373982), 10],
             // '1 watt hour is equal to 1 megaelectronvolt' => [$wh, new Megaelectronvolt(1.0), 0],
         ];
     }
